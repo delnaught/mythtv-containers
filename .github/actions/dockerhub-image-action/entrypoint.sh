da76f42627ea5910b952c5106ca6de3702f88902
delnaught <dmaincrash@yahoo.com>
2021-12-26 14:23:20 -0500

    query-dockerhub clean up entrypoint script

--- .github/actions/dockerhub-image-action/entrypoint.sh
+++ .github/actions/dockerhub-image-action/entrypoint.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 
-repo=$1
-tag=$2
-echo "::debug input ${repo}:${tag}"
-token_rest=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${repo}:pull")
-echo "::debug rtn => $? token_rest => ${token_rest}"
+repo="$1"
+tag="$2"
 token=$(echo $token_rest | jq -r '.token')
-#token=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${repo}:pull" | jq -r '.token')
-echo "::debug rtn => $? token => ${token}"
+token=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${repo}:pull" | jq -r '.token')
+echo "::debug token => ${token}"
 digest=$(curl -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
               -H "Authorization: Bearer $token" \
               -s "https://registry-1.docker.io/v2/${repo}/manifests/${tag}" \
              | jq -r '.config.digest')
-echo "::debug rtn => $? digest => ${digest}"
+echo "::debug digest => ${digest}"
 http_code=$(curl -o /dev/null --silent -Iw '%{http_code}' \
 		 -H "Authorization: Bearer $token" \
 		 -s -L "https://registry-1.docker.io/v2/${repo}/blobs/${digest}")
-echo "::debug rtn => $? http_code => ${http_code}"
+echo "::debug http_code => ${http_code}"
 echo "::set-output name=http-code::${http_code}"
 
