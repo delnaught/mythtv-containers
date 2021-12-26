9e35b898cbc46b2d38e7b4d54982c161ea522c65
delnaught <dmaincrash@yahoo.com>
2021-12-26 14:04:43 -0500

    query-dockerhub verify curl input and return codes

--- .github/actions/dockerhub-image-action/entrypoint.sh
+++ .github/actions/dockerhub-image-action/entrypoint.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-repo="$1"
-tag="$2"
+repo=$1
+tag=$2
+echo "::debug input ${repo}:${tag}"
 token=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${repo}:pull" | jq -r '.token')
-echo "::debug token => ${token}"
+echo "::debug rtn => $? token => ${token}"
 digest=$(curl -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
               -H "Authorization: Bearer $token" \
               -s "https://registry-1.docker.io/v2/${repo}/manifests/${tag}" \
              | jq -r '.config.digest')
-echo "::debug digest => ${digest}"
+echo "::debug rtn => $? digest => ${digest}"
 http_code=$(curl -o /dev/null --silent -Iw '%{http_code}' \
 		 -H "Authorization: Bearer $token" \
 		 -s -L "https://registry-1.docker.io/v2/${repo}/blobs/${digest}")
-echo "::debug http_code => ${http_code}"
+echo "::debug rtn => $? http_code => ${http_code}"
 echo "::set-output name=http-code::${http_code}"
 
