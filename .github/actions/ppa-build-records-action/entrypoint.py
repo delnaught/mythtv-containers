3fb5a812af5aa0773e299afd2bccb5a8c6cce189
delnaught <dmaincrash@yahoo.com>
2021-12-26 11:26:45 -0500

    query-dockerhub update action to output json after parsing

--- .github/actions/ppa-build-records-action/entrypoint.py
+++ .github/actions/ppa-build-records-action/entrypoint.py
@@ -1,3 +1,4 @@
+import json
 import launchpadlib
 import re
 import sys
@@ -8,14 +9,14 @@ from lazr.restfulclient.errors import HTTPError
 record_re = re.compile(r"\.(?P<date>[0-9]+)\.(?P<commit>[0-9a-f]+)~ubuntu(?P<os>[0-9]+\.[0-9]+)\.[0-9]$")
 
 def tokenize_record(record):
-    class build: pass
+    build = {}
     match = record_re.search(record.source_package_version)
     if None != match:
-        build.version = record.source_package_version
-        build.arch = record.arch_tag
-        build.date = match.group("date")
-        build.commit = match.group("commit")
-        build.os = match.group("os")
+        build["version"] = record.source_package_version
+        build["arch"] = record.arch_tag
+        build["date"] = match.group("date")
+        build["commit"] = match.group("commit")
+        build["os"] = match.group("os")
     return build
 
 ppa_name = sys.argv[1]
@@ -29,11 +30,11 @@ try:
     ppa = mythbuntu.getPPAByName(name=ppa_name)
     all_records = ppa.getBuildRecords(build_state = "Successfully built")[:records]
     arch_records = (record for record in all_records if record.arch_tag == arch)
-    arch_builds = (tokenize_record(record) for record in arch_records)
-    versions = (build.version for build in arch_builds if build.os == release)
-    json = [f"\"{version}\"" for version in versions]
-    print(f"::set-output name=package-versions::{str(json)}")
-    print(f"::set-output name=latest-version::{json[0]}")
+    arch_builds = (tokenize_record(record) for record in all_records)
+    os_builds = [build for build in arch_builds if release == build["os"]]
+    index_builds = [os_builds[index].update({ "index": index}) or os_builds[index] for index in range(len(os_builds))]
+    json = json.dumps(index_builds)
+    print(f"::set-output name=build-records::{json}")
 
 except HTTPError as e:
     print(f"::set-output name=package-versions::[]")
